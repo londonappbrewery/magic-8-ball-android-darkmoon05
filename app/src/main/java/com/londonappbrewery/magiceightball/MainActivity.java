40501b3708c75d252804b2dc8e2a873d58875ced
von geordan <von_geordan@yahoo.com>
2017-09-28 21:42:11 +0800

    yes im done

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,15 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ViewPropertyAnimatorCompatSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_roll;
+        btn_roll = (Button) findViewById(R.id.btn_roll);
+
+       final ImageView ball_for_you;
+        ball_for_you = (ImageView) findViewById(R.id.ball_for_you);
+
+        final int [] ball={
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5,
+        };
+
+        btn_roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Random randomNumberGenerator = new Random();
+                int number=randomNumberGenerator.nextInt(5);
+
+                ball_for_you.setImageResource(ball[number]);
+                Log.d("dd","press ko na!!"+number);
+
+            }
+        });
+
     }
 }
